e27c0ddf230925d4def0d048dba7eb7fdbc72319
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2026-02-06 17:45:51 -0300

    style: padronizar aspas com eslint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calcularDesconto(valor, percentual) {
   // Valida se os parâmetros são números
-  if (typeof valor !== "number" || typeof percentual !== "number") {
+  if (typeof valor !== 'number' || typeof percentual !== 'number') {
     return 0;
   }
 
@@ -9,6 +9,6 @@ function calcularDesconto(valor, percentual) {
 }
 
 // Exportação híbrida (Navegador + Node)
-if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = { calcularDesconto }; // Exportando como objeto para bater com seu require
 }
