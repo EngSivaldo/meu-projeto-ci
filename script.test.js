e0f09f61ac71c238a12c3dc3a6fa7f0595ac43cc
EngSivaldo <sivaldovieiradealmeida@gmail.com>
2026-02-06 21:36:24 -0300

    style: corrige padronização de aspas no script.test.js

--- script.test.js
+++ script.test.js
@@ -1,11 +1,11 @@
-const calcularDivida = require("./script");
+const calcularDivida = require('./script');
 
-test("Deve calcular o montante de R$ 1000 a 10% por 2 meses corretamente", () => {
+test('Deve calcular o montante de R$ 1000 a 10% por 2 meses corretamente', () => {
   // Cálculo esperado: 1000 * (1.1)^2 = 1210
   expect(calcularDivida(1000, 10, 2)).toBe(1210);
 });
 
-test("Deve retornar 0 para valores de entrada inválidos", () => {
+test('Deve retornar 0 para valores de entrada inválidos', () => {
   expect(calcularDivida(-100, 5, 1)).toBe(0);
   expect(calcularDivida(100, -2, 1)).toBe(0);
 });
